447d8ba37b612de17444a1ed5e7ffe628a646c59
Jacob Horstmeier <37892205+Dafluffinmuffin@users.noreply.github.com>
2018-06-12 16:06:20 -0600

    first commit

--- Artist.js
+++ Artist.js
@@ -1,9 +1,30 @@
+require('dotenv').config();
 var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+
+const {
+    USERNAME,
+    PASSWORD
+} = process.env;
+
 
 var personalityInsights = new PersonalityInsightsV3({
   version: '2017-10-13',
-  username: '132c9727-eaf0-4439-ba20-f810c05b34ce',
-  password: 'caidJMGcPZ4j',
+  username: 'USERNAME',
+  password: 'PASSWORD',
   url: 'https://gateway.watsonplatform.net/personality-insights/api'
   
 });
+
+var profileParams = {
+    content: require('./ColdplayAlbumTracks/sampleAllTracks.txt'),
+    content_type:'text/plain',
+    consumption_preferences: true
+};
+
+personalityInsights.profile(profileParams, function(error, profile) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.stringify(profile, null, 2));
+    }
+  });
