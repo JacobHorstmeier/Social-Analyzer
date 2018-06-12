97a7cac738a36ed5d3170a329dfaea2b9cfcb6c1
Jacob Horstmeier <37892205+Dafluffinmuffin@users.noreply.github.com>
2018-06-12 15:35:57 -0600

    added coldplay album sample data

---
+++ Artist.js
@@ -0,0 +1,9 @@
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+
+var personalityInsights = new PersonalityInsightsV3({
+  version: '2017-10-13',
+  username: '132c9727-eaf0-4439-ba20-f810c05b34ce',
+  password: 'caidJMGcPZ4j',
+  url: 'https://gateway.watsonplatform.net/personality-insights/api'
+  
+});
