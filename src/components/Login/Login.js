a5b3df0483505757b494c8898dc1d3950e5b7d7e
Jacob Horstmeier <37892205+Dafluffinmuffin@users.noreply.github.com>
2018-07-02 11:10:44 -0600

    updated login styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,12 +5,14 @@ import './Login.css';
 export default class Login extends Component {
     render() {
         return (
-            <div className='App'>
+            <div className='upper'>
+            
                 {/* <img src={logo} alt=""/>
                 using an a or anchor tag we can specify a hypertext reference. by giving it a href attribute we can change the user's location */}
                 <a href={process.env.REACT_APP_LOGIN}>
                     <button>Login</button>
                 </a>
+                
             </div> 
         )
     }
