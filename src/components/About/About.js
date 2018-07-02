6fdbafab19bced085deb3a1b72c81c02fa761647
Jacob Horstmeier <37892205+Dafluffinmuffin@users.noreply.github.com>
2018-07-02 11:18:31 -0600

    updated missing content in about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,7 +15,7 @@ export default class About extends Component {
                 <h3>How are results obtained?</h3>
                 <p>This web application uses IBM's Watson Personality Profile api to analyze the most recent album of the artist inputed and returns a profile based on the Big 5 Ocean Traits</p>
                 <h3>What does the Big 5 Mean?</h3>
-                <p>The Big Five is a research-driven approach in psychology which derives from the notion that the most common personality traits can be captured by five core dimensions: Openness to Experience. Conscientiousness. Extroversion. Agreeableness.</p>
+                <p>The Big Five is a research-driven approach in psychology which derives from the notion that the most common personality traits can be captured by five core dimensions: Openness to Experience. Conscientiousness. Extroversion. Agreeableness. Neuroticism</p>
                 </div>
             </div>
             </div>
